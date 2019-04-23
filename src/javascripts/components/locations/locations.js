b99e244e068171d23bf64eee864e1302bc0a2f1e
Saul Solano <saul3d@yahoo.com>
2019-04-22 21:22:38 -0500

    search and filter function

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -5,21 +5,90 @@ import './locations.scss';
 
 let locations = [];
 
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-success';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-info';
+      break;
+    case 'After':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+      selectedClass = '';
+  }
+  return selectedClass;
+};
+
 const domStringBuilder = () => {
   let domString = '';
   locations.forEach((location) => {
-    domString += `<h3>${location.name}</h3>`;
+    domString += `<div id="${location.id}" class="card movie" style="width: 18rem;">`;
+    domString += `<div class="card-header" ${shootTimeClass(location.shootTime)}}></div>`;
+    domString += '<div class="card-body">';
+    domString += `<h5 class="card-title">${location.name}</h5>`;
+    domString += `<img src="${location.imageUrl}">`;
+    domString += `<p class="card-text">${location.address}</p>`;
+    domString += `<div><em>${location.shootTime}</em></div>`;
+    domString += '</div>';
+    domString += '</div>';
   });
 
   util.printToDom('locations', domString);
 };
 
+const filterButtonEvent = (e) => {
+  const buttonId = e.target.id;
+  const darkLocations = locations.filter(x => x.shootTime === 'After Dark');
+  const morningLocations = locations.filter(x => x.shootTime === 'Morning');
+  const afternoonLocations = locations.filter(x => x.shootTime === 'Afternoon');
+  const eveningLocations = locations.filter(x => x.shootTime === 'Evening');
+  switch (buttonId) {
+    case 'morning':
+      domStringBuilder(morningLocations);
+      break;
+    case 'afternoon':
+      domStringBuilder(afternoonLocations);
+      break;
+    case 'evening':
+      domStringBuilder(eveningLocations);
+      break;
+    case 'dark':
+      domStringBuilder(darkLocations);
+      break;
+    default:
+      domStringBuilder(locations);
+  }
+};
+
+const filterByTextEvent = (e) => {
+  const searchText = e.target.value;
+  const searchLocations = locations.filter((x) => {
+    const hasName = x.name.includes(searchText);
+    const hadAddress = x.address.includes(searchText);
+    return hasName || hadAddress;
+  });
+  domStringBuilder(searchLocations);
+};
+
 const initializeLocations = () => {
   locationsData.getLocationsData()
-    .then((response) => {
-      const locationResults = response.data.locations;
+    .then((resp) => {
+      const locationResults = resp.data.locations;
       locations = locationResults;
-      domStringBuilder();
+      domStringBuilder(locations);
+      document.getElementById('dark').addEventListener('click', filterButtonEvent);
+      document.getElementById('afternoon').addEventListener('click', filterButtonEvent);
+      document.getElementById('evening').addEventListener('click', filterButtonEvent);
+      document.getElementById('morning').addEventListener('click', filterButtonEvent);
+      document.getElementById('all').addEventListener('click', filterButtonEvent);
+      document.getElementById('search-input').addEventListener('keyup', filterByTextEvent);
     })
     .catch(err => console.error(err));
 };
